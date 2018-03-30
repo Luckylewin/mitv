be97aab4d5abca0a260b0b16f72a809395a9fac6
Luckylewin <876505905@qq.com>
2018-03-30 10:15:53 +0800

    修改邮箱等配置文件

--- common/config/params.php
+++ common/config/params.php
@@ -17,6 +17,30 @@ return [
         'ACCESS_KEY' => '', // KEY
         'ENDPOINT'=>'oss-cn-shenzhen.aliyuncs.com',//指定区域
         'BUCKET'=>'',//bucket
-    ]
+    ],
+
+    'mailer' => [
+        'class' => 'yii\swiftmailer\Mailer',
+        'viewPath' => '@common/mail',
+        // send all mails to a file by default. You have to set
+        // 'useFileTransport' to false and configure a transport
+        // for the mailer to send real emails.
+        'useFileTransport' => false,
+        'transport' => [
+            'class' => 'Swift_SmtpTransport',
+            'host' => 'smtp.qq.com',
+            'username' => 'xxxx@foxmail.com',
+            'password' => 'xxxxxxxxxx',
+            'port' => '465',
+            'encryption' => 'ssl',
+        ],
+        'messageConfig'=>[
+            'charset'=>'UTF-8',
+            'from'=>['xxxxx@foxmail.com'=>'xxx'],
+
+        ],
+    ],
+
+
 
 ];
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -40,27 +40,7 @@ return [
             'errorAction' => 'site/error',
         ],
 
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => false,
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.qq.com',
-                'username' => 'zystyle@foxmail.com',
-                'password' => 'zdkfnusxrjzwbfgg',
-                'port' => '465',
-                'encryption' => 'ssl',
-            ],
-            'messageConfig'=>[
-                'charset'=>'UTF-8',
-                'from'=>['zystyle@foxmail.com'=>'TV APP'],
 
-            ],
-        ],
 
         /*
         'urlManager' => [
