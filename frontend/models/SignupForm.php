390bc18981b3ade8ce42b9c46cec6085523feb92
liziying <876505905@qq.com>
2018-05-09 15:57:36 +0800

    修改邮箱唯一验证

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -29,7 +29,6 @@ class SignupForm extends Model
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
 
             ['password', 'required'],
         ];
